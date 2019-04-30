9f463aef6be7c8d7cdc395b29c9854e8bc7e1566
llUJO <jirapan_yankhan@hotmail.com>
2019-04-30 22:31:15 +0700

    [ EDIT ] Add puppeteer

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-import config from './config/config';
+import config from "./config/config";
 
 const express = require("express");
+const puppeteer = require("puppeteer");
 const app = express();
 
-app.get("/",(req,res) =>{
+app.get("/", (req, res) => {
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto("http://www.gatherproxy.com/proxylist/country/?c=Thailand");
 
-    res.send('Hello world')
+    const proxyData = await page.evaluate(() => {
+      const tables = Array.from(
+        document.querySelectorAll("#tblproxy > tbody > tr ")
+      );
 
-})
+      return proxy = tables.map(proxyRes => {
+          return proxyRes.innerHTML;
+      });
+    });
+    let temp;
+    for (let i = 0; i < proxyData.length; i++) {  
+        temp = proxyData[i].split(" ");
+        console.log(temp);
+    }
+        // console.log(proxyData);
+    // res.send(proxyData);
 
-app.listen(config.port,()=>{
-    console.log("Proxy Server api started port :",config.port);
-})
+    await browser.close();
+  })();
+});
+
+app.listen(config.port, () => {
+  console.log("Proxy Server api started port :", config.port);
+});
